99d7c6ee433205c1cb1062ad35e191a8a4b0ad68
Thomas Bernard <tbernard2000.retonfey@gmail.com>
2025-05-20 00:59:48 +0200

    script to create superadmin user

---
+++ backend/prisma/seed.ts
@@ -0,0 +1,36 @@
+import { PrismaClient } from '../generated/prisma';
+import * as bcrypt from 'bcrypt';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const existing = await prisma.account.findFirst({
+    where: { isSuperadmin: true },
+  });
+  if (existing) {
+    console.log('Super admin already exists.');
+    return;
+  }
+
+  const password = process.env.SUPERADMIN_PASSWORD;
+  const name = process.env.SUPERADMIN_NAME;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  await prisma.account.create({
+    data: {
+      name: name,
+      password: hashedPassword,
+      isSuperadmin: true,
+    },
+  });
+  console.log('✅ Super admin created!');
+}
+
+main()
+  .catch((e) => {
+    console.error('❌ Error seeding:', e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
