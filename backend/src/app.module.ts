1ad6a0cd1cde6302238926c8151b38704dbe6a47
Thomas Bernard <tbernard2000.retonfey@gmail.com>
2025-05-20 03:10:13 +0200

    fix setup back cors/prisma etc

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+import { AuthModule } from './auth/auth.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { AppController } from './app.controller';
+
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'static'),
+      exclude: ['/api*'],
+    }),
+    AuthModule,
+    PrismaModule,
+  ],
+  controllers: [AppController],
 })
 export class AppModule {}
