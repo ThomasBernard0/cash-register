f89cff3f77dfd13472f4751445ef24e10ba9245a
Thomas Bernard <tbernard2000.retonfey@gmail.com>
2025-05-23 18:12:36 +0200

    accounthubpage

--- frontend/src/pages/account/AccountHubPage.tsx
+++ frontend/src/pages/account/AccountHubPage.tsx
@@ -1,8 +1,19 @@
 import React from "react";
 import SectionGrid from "../../components/account/SectionGrid";
+import { useSections } from "../../api/section";
 
 const AccountHubPage: React.FC = () => {
-  return <></>;
+  const { sections, loading, error } = useSections();
+
+  if (loading) {
+    return <div>Loading sections...</div>;
+  }
+
+  if (error) {
+    return <div style={{ color: "red" }}>{error}</div>;
+  }
+
+  return <SectionGrid sections={sections} />;
 };
 
 export default AccountHubPage;
