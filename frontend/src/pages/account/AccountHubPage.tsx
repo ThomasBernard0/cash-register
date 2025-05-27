9ab15080287b56e649cdfb98bd1ca658eef5231c
Thomas Bernard <tbernard2000.retonfey@gmail.com>
2025-05-27 22:43:05 +0200

    accounthubpage style

--- frontend/src/pages/account/AccountHubPage.tsx
+++ frontend/src/pages/account/AccountHubPage.tsx
@@ -1,18 +1,59 @@
 import { useNavigate } from "react-router-dom";
+import { Box, Button, Typography, Paper } from "@mui/material";
 
 const AccountHubPage: React.FC = () => {
   const navigate = useNavigate();
 
   return (
-    <div>
-      <h1>Page Hub</h1>
-      <button onClick={() => navigate("/account/view")}>
-        Aller à Page View
-      </button>
-      <button onClick={() => navigate("/account/edit")}>
-        Aller à Page Edit
-      </button>
-    </div>
+    <Box
+      sx={{
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        bgcolor: "#f5f5f5",
+        px: 2,
+      }}
+    >
+      <Paper
+        elevation={4}
+        sx={{
+          p: 4,
+          borderRadius: 4,
+          textAlign: "center",
+        }}
+      >
+        <Typography variant="h4" sx={{ marginBottom: 10 }}>
+          Bienvenue sur votre espace
+        </Typography>
+
+        <Box sx={{ display: "flex", flexDirection: "column", gap: 4 }}>
+          <Button
+            variant="contained"
+            size="large"
+            onClick={() => navigate("/account/view")}
+          >
+            Voir le menu
+          </Button>
+
+          <Button
+            variant="contained"
+            size="large"
+            onClick={() => navigate("/account/edit")}
+          >
+            Modifier le menu
+          </Button>
+
+          <Button
+            variant="contained"
+            size="large"
+            onClick={() => navigate("/account/session")}
+          >
+            Modifier la session
+          </Button>
+        </Box>
+      </Paper>
+    </Box>
   );
 };
 
