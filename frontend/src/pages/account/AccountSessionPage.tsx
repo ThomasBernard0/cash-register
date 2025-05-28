e7908cc9d7b90d832480ff9416f1bc812511abc7
Thomas Bernard <tbernard2000.retonfey@gmail.com>
2025-05-28 18:30:22 +0200

    sessionpage style

--- frontend/src/pages/account/AccountSessionPage.tsx
+++ frontend/src/pages/account/AccountSessionPage.tsx
@@ -1,4 +1,4 @@
-import { Box, Typography, Button, Grid } from "@mui/material";
+import { Box, Typography, Button, Grid, Paper } from "@mui/material";
 import AccountNavbar from "../../components/account/AccountNavbar";
 
 const AccountSessionPage = () => {
@@ -6,32 +6,36 @@ const AccountSessionPage = () => {
     <>
       <AccountNavbar />
       <Box
-        sx={{
-          p: 4,
-          maxWidth: 400,
-          margin: "auto",
-          border: "1px solid #ccc",
-          borderRadius: 2,
-        }}
+        sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}
       >
-        <Typography variant="h5" gutterBottom align="center">
-          Mon Titre
-        </Typography>
+        <Paper
+          elevation={4}
+          sx={{
+            width: "50%",
+            mt: 6,
+            p: 4,
+            borderRadius: 4,
+          }}
+        >
+          <Typography variant="h5" sx={{ fontWeight: "bold", mb: 4 }}>
+            Editer la session
+          </Typography>
 
-        <Box sx={{ mb: 4 }}>
-          <Typography>Label 1</Typography>
-          <Typography>Label 2</Typography>
-          <Typography>Label 3</Typography>
-        </Box>
+          <Box sx={{ mb: 4, display: "flex", flexDirection: "column", gap: 2 }}>
+            <Typography>Session en cours : </Typography>
+            <Typography>CA : </Typography>
+            <Typography>Label 3</Typography>
+          </Box>
 
-        <Grid container justifyContent="space-between">
-          <Button variant="contained" color="primary">
-            Bouton Gauche
-          </Button>
-          <Button variant="outlined" color="secondary">
-            Bouton Droit
-          </Button>
-        </Grid>
+          <Grid container justifyContent="space-between">
+            <Button variant="contained" color="primary">
+              Bouton Gauche
+            </Button>
+            <Button variant="contained" color="primary">
+              Bouton Droit
+            </Button>
+          </Grid>
+        </Paper>
       </Box>
     </>
   );
