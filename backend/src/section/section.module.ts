9ccfc838019e821f09956b8ede257c428bdf577c
Thomas Bernard <tbernard2000.retonfey@gmail.com>
2025-05-28 18:52:26 +0200

    fix back import module

--- backend/src/section/section.module.ts
+++ backend/src/section/section.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { SectionController } from './section.controller';
 import { SectionService } from './section.service';
+import { AccountModule } from 'src/account/account.module';
 
 @Module({
+  imports: [AccountModule],
   providers: [SectionService],
   controllers: [SectionController],
   exports: [SectionService],
